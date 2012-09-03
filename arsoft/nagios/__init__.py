4f9fed660a50cd5a06014d608e6129bf7dd23d1e
aroth <aroth@arsoft.dyndns.dk>
2012-09-03 06:13:42 +0000

    working on nagios plugin
    git-svn-id: file:///local/nuna/developer/svn/syscfg@1207 dafa5faa-33fa-417a-9101-543bec8d0801

--- arsoft/nagios/__init__.py
+++ arsoft/nagios/__init__.py
@@ -2,27 +2,87 @@
 # -*- coding: utf-8 -*-
 
 ## This is for the custom nagios module
-from pynag.Plugins import OK, WARNING, CRITICAL, UNKNOWN,
-    check_threshold as pynag_check_threshold, 
-    check_range as pynag_check_range, 
+from pynag.Plugins import OK, WARNING, CRITICAL, UNKNOWN, \
+    check_threshold as pynag_check_threshold, \
+    check_range as pynag_check_range, \
     simple as pynagPlugin
 
 class NagiosPlugin(pynagPlugin):
+    
     def __init__(self, shortname = None, version = None, blurb = None, extra = None, url = None, license = None, plugin = None, timeout = 15, must_threshold = True):
-        super.__init__(shortname, version, blurb, extra, url, license, plugin, timeout, must_threshold)
-
-    def add_value(self, label, value=None, uom = None, warn = None, crit = None, minimum = None, maximum = None):
-        super.add_perfdata(label, value, uom, warn, crit, minimum, maximum)
+        pynagPlugin.__init__(self, shortname, version, blurb, extra, url, license, plugin, timeout, must_threshold)
         
+        self._named_values = {}
+
+    def add_value(self, label, description=None, warning=False, critical=False):
+        pynagPlugin.add_perfdata(self, label, value=None)
+
+        self.add_arg(label[0], label, description, required=None)
+        
+        self._named_values[label] = { 'description':description, 'warning': warning, 'critical': critical }
+        return None
+
     def set_value(self, label, value):
         ret = None
 
         # first update the value in the perfdata
         num_data = len(self.data['perfdata'])
-        for i < num_data:
+        i = 0
+        while i < num_data:
             if self.data['perfdata'][i]['label'] == label:
                 self.data['perfdata'][i]['value'] = value
                 ret = pynag_check_threshold(value, warning=self.data['perfdata'][i]['warn'], critical=self.data['perfdata'][i]['crit'])
                 break
+            i = i + 1
         return ret
+    def activate(self):
+        pynagPlugin.activate(self)
+
+        # first update the value in the perfdata
+        num_data = len(self.data['perfdata'])
+        i = 0
+        while i < num_data:
+            label_name = self.data['perfdata'][i]['label']
+            label_value = self[label_name]
+            if label_value is not None:
+                for label, named_value_data in self._named_values.items():
+                    if label_name == label:
+                        if named_value_data['warning']:
+                            self.data['perfdata'][i]['warn'] = label_value
+                        if named_value_data['critical']:
+                            self.data['perfdata'][i]['crit'] = label_value
+                        break
+            i = i + 1
+
+    def _check_threshold(self, value_item):
+        print('_check_threshold ' + str(value_item))
+        code = pynag_check_threshold(value_item['value'], warning=value_item['warn'], critical=value_item['crit'])
+        if code == OK:
+            msg = None
+        elif code == WARNING:
+            msg = '%s outside specified range %s' % (value_item['label'], value_item['warn'])
+        elif code == CRITICAL:
+            msg = '%s outside specified range %s' % (value_item['label'], value_item['crit'])
+        else:
+            msg = None
+        return (code, msg)
+        
+    def check_values(self):
+        # first update the value in the perfdata
+        num_data = len(self.data['perfdata'])
+        i = 0
+        exit_code = OK
+        exit_message = ''
+        while i < num_data:
+            (ret, msg) = self._check_threshold(self.data['perfdata'][i])
+            if ret != OK:
+                print(str(self.data['perfdata'][i]) + ' not ok')
+                if ret > exit_code:
+                    exit_code = ret
+                if exit_message:
+                    exit_message = exit_message + ',' + msg
+                else:
+                    exit_message = msg
+            i = i + 1
+        return (exit_code, exit_message)
     
