390039a2fc5e88c816bb9fb47eb0591a9c19ee8c
Andreas Roth <andreas.roth@fastprotect.net>
2013-01-23 16:23:50 +0100

    prepare new packages

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from distutils.core import setup
 
 setup(name='arsoft-python',
-		version='1.38',
+		version='1.39',
 		description='AR Soft Python modules',
 		author='Andreas Roth',
 		author_email='aroth@arsoft-online.com',
