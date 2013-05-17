291ffdb1474168bf955f969dbcc4367cf965c15b
Andreas Roth <aroth@arsoft-online.com>
2013-05-17 20:07:34 +0200

    remove wind direction text from check_weather start on jabber daemon

--- arsoft/xmpp/__init__.py
+++ arsoft/xmpp/__init__.py
@@ -8,7 +8,7 @@ __path__ = extend_path(__path__, __name__)
 # version of the arsoft.xmpp module
 __version__ = '1.0'
 
-from _sleekxmpp import sleekxmpp_send_message, sleekxmpp_backend_info
+from _sleekxmpp import sleekxmpp_send_message, sleekxmpp_backend_info, sleekxmpp_message_bot
 
 def send_message(sender, password, recipient, body, html=None, subject=None, message_type='chat',
                            ipv4=True, ipv6=True):
@@ -17,6 +17,10 @@ def send_message(sender, password, recipient, body, html=None, subject=None, mes
                                     ipv4=ipv4, ipv6=ipv6)
 def backend_info():
     return sleekxmpp_backend_info()
+
+def message_bot(sender, password, handler=None, ipv4=True, ipv6=True):
+    return sleekxmpp_message_bot(sender=sender, password=password, handler=handler,
+                                    ipv4=ipv4, ipv6=ipv6)
 
 def version():
     return __version__
--- arsoft/xmpp/_sleekxmpp.py
+++ arsoft/xmpp/_sleekxmpp.py
@@ -4,6 +4,10 @@
 
 from _backend import *
 
+if sys.version_info < (3, 0):
+    reload(sys)
+    sys.setdefaultencoding('utf8')
+
 def sleekxmpp_send_message(sender, password, recipient, body, html=None, subject=None, message_type='chat',
                            ipv4=True, ipv6=True):
 
@@ -69,6 +73,67 @@ def sleekxmpp_send_message(sender, password, recipient, body, html=None, subject
     xmpp.connect(use_ssl=False, use_tls=True)
     xmpp.process(block=True)
     return xmpp.message_sent
+
+def sleekxmpp_message_bot(sender, password, handler=None, ipv4=True, ipv6=True):
+
+    from sleekxmpp import ClientXMPP
+    class ChatMsgBot(ClientXMPP):
+
+        """
+        A basic SleekXMPP bot that will log in, send a message,
+        and then log out.
+        """
+
+        def __init__(self, jid, password, handler, ipv4=True, ipv6=True):
+            ClientXMPP.__init__(self, jid, password)
+
+            self.use_ipv6 = ipv6
+            # The message we wish to send, and the JID that
+            # will receive it.
+            self.handler = handler
+
+            # The session_start event will be triggered when
+            # the bot establishes its connection with the server
+            # and the XML streams are ready for use. We want to
+            # listen for this event so that we we can initialize
+            # our roster.
+            self.add_event_handler("session_start", self.start, threaded=True)
+
+        def start(self, event):
+            """
+            Process the session_start event.
+
+            Typical actions for the session_start event are
+            requesting the roster and broadcasting an initial
+            presence stanza.
+
+            Arguments:
+                event -- An empty dictionary. The session_start
+                        event does not provide any additional
+                        data.
+            """
+            self.send_presence()
+            self.get_roster()
+
+            self.handler('session_start', None)
+            
+        def send(self, recipient, message, subject=None, html=None, message_type='chat'):
+            self.send_message(mto=recipient,
+                              msubject=subject,
+                              mbody=message,
+                              mhtml=html,
+                              mtype=message_type)
+            
+        def close(self):
+            self.disconnect()
+
+    xmpp = ChatMsgBot(sender, password, handler, ipv4=ipv4, ipv6=ipv6)
+    xmpp.register_plugin('xep_0030') # Service Discovery
+    xmpp.register_plugin('xep_0199') # XMPP Ping
+    xmpp.connect(use_ssl=False, use_tls=True)
+    xmpp.process(block=False)
+    return xmpp
+
 
 def sleekxmpp_backend_info():
     from sleekxmpp.version import __version__ as version, __version_info__ as version_info
