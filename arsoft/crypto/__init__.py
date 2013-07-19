fa0ef8c1ee8055e19450c33433ca546915ffc051
Andreas Roth <andreas.roth@fastprotect.net>
2013-07-19 16:25:31 +0200

    add pwgen function

--- arsoft/crypto/__init__.py
+++ arsoft/crypto/__init__.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
+# kate: space-indent on; indent-width 4; mixedindent off; indent-mode python;
 
 __version__ = 0.9
 
@@ -6,5 +8,6 @@ from pem import PEMItem, PEMFile
 from cert import Certificate, CertificateList, CertificateListFile, CertificatePEMFile, CertificateFile
 from key import KeyItem, KeyList, KeyPEMFile
 from crl import CRL, CRLList, CRLPEMFile, CRLFile
+from pwgen import pwgen
 
 #print "netconfig %s" % __name__
