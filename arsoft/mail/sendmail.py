96b7c88e8e7de7939b2ab529f4fee73f059bdf51
Andreas Roth <andreas.roth@fastprotect.net>
2013-08-03 13:12:22 +0200

    add authentication support and added error handling

--- arsoft/mail/sendmail.py
+++ arsoft/mail/sendmail.py
@@ -4,15 +4,17 @@
 
 import smtplib
 
-def sendmail(mail, server='localhost', port=25, lmtp=False, ssl=False, starttls=False, debuglevel=0):
+def sendmail(mail, server='localhost', port=25, user=None, password=None, lmtp=False, ssl=False, starttls=False, debuglevel=0):
     if lmtp:
         s = smtplib.LMTP(server, port)
     elif ssl:
         s = smtplib.SMTP_SSL(server, port)
     else:
         s = smtplib.SMTP(server, port)
-        if starttls:
-            s.starttls()
     s.set_debuglevel(debuglevel)
+    if starttls:
+        s.starttls()
+    if user is not None:
+        s.login(user, password)
     s.sendmail(mail.sender, mail.recipients, str(mail))
     s.quit()
