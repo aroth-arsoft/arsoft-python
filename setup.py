34027d7f6a1ef6f6a04aad3307b9ebe990f0542d
Andreas Roth <andreas.roth@fastprotect.net>
2013-08-17 18:55:38 +0200

    next release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 from distutils.core import setup
 
 setup(name='arsoft-python',
-		version='1.88',
+		version='1.89',
 		description='AR Soft Python modules',
 		author='Andreas Roth',
 		author_email='aroth@arsoft-online.com',
