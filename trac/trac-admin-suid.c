b171b71f1d9ae8909961e8f85011782ff69fd07e
Andreas Roth <andreas.roth@fastprotect.net>
2013-09-05 10:44:32 +0200

    fixing suid trouble; implements #617

--- trac/trac-admin-suid.c
+++ trac/trac-admin-suid.c
@@ -1,34 +1,55 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+int switch_to_user()
+{
+    int ret;
+    uid_t euid = geteuid();
+    gid_t egid = getegid();
+
+    // because of the extra security of bash (and/or other shells), the shell will set the
+    // effective uid (EUID) back to the real uid (RUID), which causes the script to run
+    // without root permissions. Happens on Ubuntu 11.10.
+    // see 
+    // http://stackoverflow.com/questions/556194/calling-a-script-from-a-setuid-root-c-program-script-does-not-run-as-root
+
+    ret = setuid(euid);
+    if(ret != 0)
+    {
+        char * errorno_msg = strerror(errno);
+        fprintf(stderr, "Failed to set UID to %i, error %i %s\n", euid, errno, errorno_msg);
+    }
+    else
+    {
+        ret = setgid(egid);
+        if(ret != 0)
+        {
+            char * errorno_msg = strerror(errno);
+            fprintf(stderr, "Failed to set GID to %i, error %i %s\n", egid, errno, errorno_msg);
+        }
+    }
+    return ret;
+}
 
 int main(int argc, char ** argv, char ** env)
 {
-    const char * trac_admin = "/usr/bin/trac-admin";
-    char ** trac_admin_argv = NULL;
-    int i;
-    //int uid;
-    uid_t euid;
-    gid_t egid;
-    trac_admin_argv = (char **)malloc(sizeof(char*) * (argc + 1));
-    trac_admin_argv[0] = (char*)trac_admin;
+    const char * target_executable = "/usr/bin/trac-admin";
+    char ** target_argv = NULL;
+    int i, ret;
+
+    target_argv = (char **)malloc(sizeof(char*) * (argc + 1));
+    target_argv[0] = (char*)target_executable;
     for(i = 1; i < argc; i++)
-        trac_admin_argv[i] = argv[i];
-    trac_admin_argv[argc] = NULL;
-	// because of the extra security of bash (and/or other shells), the shell will set the
-	// effective uid (EUID) back to the real uid (RUID), which causes the script to run
-	// without root permissions. Happens on Ubuntu 11.10.
-	// see 
-	// http://stackoverflow.com/questions/556194/calling-a-script-from-a-setuid-root-c-program-script-does-not-run-as-root
-    //uid = getuid();
-    euid = geteuid();
-    egid = getegid();
-    //printf("euid=%i, egid=%i\n", euid, egid);
-	if(setuid(euid) != 0)
-        fprintf(stderr, "setuid(%i) failed\n", euid);
-    if(setgid(egid) != 0)
-        fprintf(stderr, "setgid(%i) failed\n", egid);
-    //printf("euid=%i\n", euid);
-	// start the script with the parameters
-    return execve(trac_admin, trac_admin_argv, env);
+        target_argv[i] = argv[i];
+    target_argv[argc] = NULL;
+
+    ret = switch_to_user();
+    if(!ret)
+    {
+        // start the script with the parameters
+        ret = execve(target_executable, target_argv, env);
+    }
+    return ret;
 }
