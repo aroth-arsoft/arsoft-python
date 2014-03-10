c5b078363962ddea68c8bc5b71117065cf86c3bd
Andreas Roth <aroth@arsoft-online.com>
2014-03-10 13:39:39 +0100

    prepare next version with backup script

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 from distutils.core import setup
 
 setup(name='arsoft-python',
-		version='1.108',
+		version='1.109',
 		description='AR Soft Python modules',
 		author='Andreas Roth',
 		author_email='aroth@arsoft-online.com',
