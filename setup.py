7f56dc3efaee90a1edd4678b58216048a813bf8a
Andreas Roth <aroth@arsoft-online.com>
2014-04-05 13:17:43 +0200

    add patterns and shortcuts for cups

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 from distutils.core import setup
 
 setup(name='arsoft-python',
-		version='1.122',
+		version='1.123',
 		description='AR Soft Python modules',
 		author='Andreas Roth',
 		author_email='aroth@arsoft-online.com',
