8402b58ec9be09f418d252287d92e1d8752ddecd
Andreas Roth <aroth@arsoft-online.com>
2014-04-17 18:55:13 +0200

    next version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 from distutils.core import setup
 
 setup(name='arsoft-python',
-		version='1.127',
+		version='1.128',
 		description='AR Soft Python modules',
 		author='Andreas Roth',
 		author_email='aroth@arsoft-online.com',
