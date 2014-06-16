c9770a6ec9f7434d6eb0f090ad3e08f48144a0fa
Andreas Roth <aroth@arsoft-online.com>
2014-06-16 14:35:32 +0200

    next version with openvpn fixes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 from distutils.core import setup
 
 setup(name='arsoft-python',
-		version='1.130',
+		version='1.131',
 		description='AR Soft Python modules',
 		author='Andreas Roth',
 		author_email='aroth@arsoft-online.com',
