c6f61a6f0de0da5030656b23f73c9487279bebd0
Andreas Roth <aroth@arsoft-online.com>
2015-01-21 18:40:43 +0100

    next version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def version_dep_scripts(scripts):
     return ret
 
 setup(name='arsoft-python',
-		version='1.199',
+		version='1.200',
 		description='AR Soft Python modules',
 		author='Andreas Roth',
 		author_email='aroth@arsoft-online.com',
