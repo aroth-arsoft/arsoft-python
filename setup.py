37cd71ecb74c56f74e0d3e248590908a90e0de9b
Andreas Roth <aroth@arsoft-online.com>
2016-01-04 16:53:42 +0100

    heimdal-password-expire: add notes about certificates in mail message

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def version_dep_scripts(scripts):
     return ret
 
 setup(name='arsoft-python',
-		version='1.229',
+		version='1.230',
 		description='AR Soft Python modules',
 		author='Andreas Roth',
 		author_email='aroth@arsoft-online.com',
