9794c2f4aba250ec2732a8cd84e7f1f9b759a242
Andreas Roth <aroth@arsoft-online.com>
2016-05-08 14:34:42 +0200

    eurosport-stream: better quality selection

---
+++ python2/arsoft/eurosport/Program.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# kate: space-indent on; indent-width 4; mixedindent off; indent-mode python;
+#
+# Original from https://github.com/glothriel/libeurosport
+
+from .Login import Login
+from .Constants import *
+from .Channel import Channel
+import urllib.request
+import urllib.parse
+import json
+
+
+class Program:
+
+    channels = []
+
+    @staticmethod
+    def _get_all_query_parameters():
+        return urllib.parse.urlencode({
+            "data": Login.get_instance().get_confirmation(),
+            "context": Login.get_instance().prepare_application_context()
+        })
+
+    @staticmethod
+    def get_channel_list():
+        base_url = BASE_PROGRAMS_PATH
+        base_url += Program._get_all_query_parameters()
+        raw_response = urllib.request.urlopen(base_url)
+        return Program.extract_channels_from_json(raw_response.read().decode('utf8'))
+
+    @staticmethod
+    def extract_channels_from_json(json_str):
+        data = json.loads(json_str)
+        channels = []
+        for single_channel_data in data['PlayerObj']:
+            channels.append(Channel(single_channel_data))
+        return channels
---
+++ python2/arsoft/eurosport/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# kate: space-indent on; indent-width 4; mixedindent off; indent-mode python;
+#
+# Original from https://github.com/glothriel/libeurosport
+
+__author__ = 'kosto'
