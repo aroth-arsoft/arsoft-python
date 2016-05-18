c32e742b1d54b4c6588b991cde64c5c51ec9fca4
Andreas Roth <aroth@arsoft-online.com>
2016-05-18 15:55:54 +0200

    openvpn-admin: fixed several python3 issues

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ def distribution_dep_scripts(scripts, prefix=None):
     return ret
 
 setup(name='arsoft-python',
-		version='1.246',
+		version='1.247',
 		description='AR Soft Python modules',
 		author='Andreas Roth',
 		author_email='aroth@arsoft-online.com',
