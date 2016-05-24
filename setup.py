2d6f90e78a79f57b6eb5767a5c3703518a32637b
Andreas Roth <aroth@arsoft-online.com>
2016-05-24 18:19:15 +0200

    * openvpn-admin: fixed password file   * openvpn-admin: allow to disable private key encryption

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ def distribution_dep_scripts(scripts, prefix=None):
     return ret
 
 setup(name='arsoft-python',
-		version='1.248',
+		version='1.249',
 		description='AR Soft Python modules',
 		author='Andreas Roth',
 		author_email='aroth@arsoft-online.com',
