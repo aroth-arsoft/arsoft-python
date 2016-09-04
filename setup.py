efe1e6a4d6f805f8e46ff7193c32a80bc2deab58
Andreas Roth <aroth@arsoft-online.com>
2016-09-04 13:49:30 +0200

    support more plugins

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ def distribution_dep_scripts(scripts, prefix=None):
     return ret
 
 setup(name='arsoft-python',
-		version='1.270',
+		version='1.271',
 		description='AR Soft Python modules',
 		author='Andreas Roth',
 		author_email='aroth@arsoft-online.com',
