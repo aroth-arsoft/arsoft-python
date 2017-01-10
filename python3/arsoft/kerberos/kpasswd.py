9e67e985aba9f37632b059543566fab2dc28794a
Andreas Roth <aroth@arsoft-online.com>
2017-01-10 19:36:07 +0100

    get kpasswd function working with MIT version of kpasswd and adjust for python3, but needs further testing with heimdal

--- python3/arsoft/kerberos/kpasswd.py
+++ python3/arsoft/kerberos/kpasswd.py
@@ -5,53 +5,77 @@
 import pexpect
 import sys
 
-def kpasswd(principal, oldpassword, newpassword, verbose=False):
-    child = pexpect.spawn('/usr/bin/kpasswd', [principal], env={'LANG':'C'})
-    if verbose:
-        child.logfile = sys.stdout
+class _kpasswd_output(object):
+    def __init__(self, verbose=False):
+        self._verbose = False
+        self._messages = []
+    def write(self, s):
+        if self._verbose:
+            print(s)
+        self._messages.append(s.decode('utf8').strip())
+    def flush(self):
+        pass
+
+    @property
+    def message(self):
+        return '\n'.join(self._messages)
+
+    @property
+    def last_message(self):
+        return self._messages[-1]
+
+def kpasswd(principal, oldpassword, newpassword, verbose=False, timeout=5):
+    ret = False
+    error_message = None
+    child = pexpect.spawn('/usr/bin/kpasswd', [principal], env={'LANG':'C'}, timeout=5)
+    child.logfile = _kpasswd_output(verbose=verbose)
     if child:
-        if child.expect(['[Pp]assword:', pexpect.EOF]) == 0:
+        if child.expect(['[Pp]assword.*:', pexpect.EOF]) == 0:
             child.sendline(oldpassword)
-            if child.expect(['New password', pexpect.EOF]) == 0 and \
+            if child.expect(['(New password|Enter new password)', pexpect.EOF]) == 0 and \
                 child.expect([':', pexpect.EOF]) == 0:
                 child.sendline(newpassword)
-                if child.expect(['Verify password - New password', pexpect.EOF]) == 0 and \
+                if child.expect(['(Verify password - New password|Enter it again)', pexpect.EOF]) == 0 and \
                     child.expect([':', pexpect.EOF]) == 0:
                     child.sendline(newpassword)
                     child.wait()
-                    last_message = child.read().strip()
+                    last_message = child.read().strip().decode('utf8')
+                    error_message = last_message
                     child.close()
                     ret = True if child.exitstatus == 0 else False
                     if ret:
-                        (status, message) = last_message.split(':')
-                        status = status.strip()
-                        if status == 'Success':
+                        if ':' in last_message:
+                            (status, message) = last_message.split(':')
+                            status = status.strip()
+                            if status == 'Success':
+                                ret = True
+                            else:
+                                error_message = message.strip()
+                        elif 'Password changed':
                             ret = True
-                            error_message = None
-                        else:
-                            ret = False
-                            error_message = message.strip()
+                    else:
+                        pass
                 else:
+                    error_message = 'New password was not accepted: %s' % child.logfile.last_message
                     child.close()
-                    ret = False
-                    error_message = 'New password was not accepted.'
             else:
+                error_message = 'Old password was not accepted: %s' % child.logfile.last_message
                 child.close()
-                ret = False
-                error_message = 'Old password was not accepted.'
         else:
+            error_message = child.logfile.last_message
             child.close()
-            ret = False
-            error_message = 'Unexpected output from kpasswd.'
     else:
-        ret = False
         error_message = 'Failed to start kpasswd tool.'
     return (ret, error_message)
 
 if __name__ == '__main__':
-    (ret, error_message) = kpasswd('tux', 'tuxer0', 'tuxedo', verbose=True)
-    print(ret, error_message)
-    (ret, error_message) = kpasswd('tux', 'tuxedo', 'tuxer0', verbose=True)
-    print(ret, error_message)
-    (ret, error_message) = kpasswd('tux', 'tuxedo', 'tuxer0', verbose=True)
-    print(ret, error_message)
+    if len(sys.argv) == 4:
+        (ret, error_message) = kpasswd(sys.argv[1], sys.argv[2], sys.argv[3], verbose=True)
+        print(ret, error_message)
+    else:
+        (ret, error_message) = kpasswd('tux', 'tuxer0', 'tuxedo', verbose=True)
+        print(ret, error_message)
+        (ret, error_message) = kpasswd('tux', 'tuxedo', 'tuxer0', verbose=True)
+        print(ret, error_message)
+        (ret, error_message) = kpasswd('tux', 'tuxedo', 'tuxer0', verbose=True)
+        print(ret, error_message)
